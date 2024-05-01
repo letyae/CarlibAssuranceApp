092f5356febf069aa783e1e17b112aa94665e5ff
letekoma Yao <67656129+letyae@users.noreply.github.com>
2024-05-01 23:53:57 +0200

    Update ProductService.java

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,5 +1,4 @@
 package service;
-<<<<<<< HEAD
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,11 +14,5 @@ public class ProductService {
  public Iterable<Product> getProducts(){
     return productRepository.findAll();
   }
-=======
-import org.springframework.stereotype.Service;
 
-@Service
-public class ProductService {
-
->>>>>>> cbf46cbb80b4bf03d8d04d7f18d93ea0f1a7c836
 }
