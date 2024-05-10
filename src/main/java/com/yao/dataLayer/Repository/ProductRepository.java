eadbea343e459b65dc53517394515871867a808a
Yae <letekomayao@gmail.com>
2024-05-10 18:44:59 +0200

    Creation de methode/ requete native et derive dans productRepository

--- src/main/java/com/yao/dataLayer/Repository/ProductRepository.java
+++ src/main/java/com/yao/dataLayer/Repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.yao.dataLayer.Repository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Param;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,15 @@ import com.yao.dataLayer.model.Product;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
-
+	
+	public Iterable<Product> findByName(String Name);
+	public Iterable<Product> findByCategoriesName(String Name);
+	
+	//requette JPQL
+	@Query("FROM PRODUCT WHERE name=?1")
+	public Iterable<Product> findByNameJPQL(String Name);
+	
+	//requette Native
+	@Query(value="SELECT * FROM PRODUCT WHERE cout=:cout", nativeQuery=true);
+	public Iterable<Product> findByName(@Param('cout') Integer cost);
 }
